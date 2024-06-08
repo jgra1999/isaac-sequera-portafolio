5bea84952c7f1a5ea3700473cbfeb948857a9bf5
Jose Rojas <jgra11.2010@gmail.com>
2024-06-08 17:06:18 -0400

    project init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Hero from './components/Hero.vue'
+import About from './components/About.vue'
+import DesignsList from './components/DesignsList.vue'
+</script>
+
+<template>
+	<main>
+		<Hero />
+
+		<About />
+
+		<DesignsList />
+	</main>
+</template>
