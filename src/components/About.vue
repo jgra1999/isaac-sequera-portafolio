19a81d140a571f8adbe047b89c939b6fbfbe784e
Jose Rojas <jgra11.2010@gmail.com>
2024-06-08 17:07:06 -0400

    about section created

---
+++ src/components/About.vue
@@ -0,0 +1,29 @@
+<template>
+	<section id="sectionPin">
+		<div class="pin-wrap-sticky">
+			<div class="pin-wrap">
+				<div class="about__description">
+					<h2>Hola! Soy Isaac Sequera</h2>
+					<p>
+						Soy un diseñador gráfico apasionado por crear composiciones visuales
+						impactantes y efectivas.
+					</p>
+					<p>
+						Me gusta hacer diseños que ayuden a comunicar mensajes de manera clara y
+						atractiva. Mi enfoque es siempre profesional, comprometido y eficaz,
+						garantizando resultados de alta calidad.
+					</p>
+				</div>
+				<div class="about__img__container">
+					<img src="/about-1.jpg" alt="" />
+				</div>
+				<div class="about__img__container">
+					<img src="/about-2.jpg" alt="" />
+				</div>
+				<div class="about__img__container">
+					<img src="/about-3.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
